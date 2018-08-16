c295d7c19aadd283dd6e980260e4005afe829ca5
St00per <kirillshteffen@gmail.com>
2018-08-16 12:18:28 +0600

    URL request string replace with struct

--- FlickrViewer/FlickrViewer/SearchViewController.swift
+++ FlickrViewer/FlickrViewer/SearchViewController.swift
@@ -12,26 +12,22 @@ import Alamofire
 class SearchViewController: UIViewController, UISearchBarDelegate {
     
     var photos: [Photo] = []
-    var userId: String? = nil
-    private let flickrApiKey = "1ebbbfd26e664bd73f3dd4f88153e6e3"
-    
     
     //MARK- Search
-    @IBOutlet weak var searchBar: UISearchBar!
     
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var collectionView: UICollectionView!
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         flickrPhotoSearching(searchText: searchBar.text!)//make throwable
-        
-        
     }
+    
     
     private func flickrPhotoSearching (searchText: String) {
         let escapedSearchText: String = searchText.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
-        let requestUrl: String = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(flickrApiKey)&tags=\(escapedSearchText)&extras=url_o,url_m,owner_name,date_upload,views,icon_server&sort=relevance&per_page=50&format=json&nojsoncallback=1"
-        
-        Alamofire.request(requestUrl).responseJSON{response in
+        let requestUrl = FlickrURL()
+        let urlString = requestUrl.baseUrl+requestUrl.method+requestUrl.apiKey+requestUrl.searchTags+("\(escapedSearchText)")+requestUrl.extras+requestUrl.sort+requestUrl.photosPerPage+requestUrl.format
+        Alamofire.request(urlString).responseJSON{response in
             guard let photoData = response.data else{return}
             let flickrPhotos = try! JSONDecoder().decode(FlickrPhotos.self, from: photoData)
             print(flickrPhotos)
@@ -41,17 +37,11 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
             self.collectionView.reloadData()
         })
     }
-    
-    
 }
 
 extension SearchViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
     
     // MARK: UICollectionViewDataSource
-    
-    var flowLayout: UICollectionViewFlowLayout? {
-        return collectionView?.collectionViewLayout as? UICollectionViewFlowLayout
-    }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return photos.count
@@ -63,14 +53,22 @@ extension SearchViewController: UICollectionViewDataSource, UICollectionViewDele
         return cell
     }
     
+    var flowLayout: UICollectionViewFlowLayout? {
+        return collectionView?.collectionViewLayout as? UICollectionViewFlowLayout
+    }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout:UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
         guard let photoHeight = Float(photos[indexPath.row].height_m) else {return CGSize(width: 0.0, height: 0.0)}
         guard let photoWidth = Float(photos[indexPath.row].width_m) else {return CGSize(width: 0.0, height: 0.0)}
-        let aspectRatio = (photoHeight/photoWidth)
-        if let height = flowLayout?.itemSize.height{
-            return CGSize(width: height/CGFloat(aspectRatio), height: height)
+        let aspectRatio = (photoWidth/photoHeight)
+        
+        
+        
+        if let width = flowLayout?.itemSize.width{
+            return CGSize(width: width, height: width/CGFloat(aspectRatio))
         }
+        
         return CGSize(width: 0.0, height: 0.0)
     }
 }
