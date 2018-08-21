d86acf857e202b101cadb057478e2a962b3a2f12
Kirill Shteffen <kirillshteffen@gmail.com>
2018-08-21 11:36:12 +0600

    new init commit

---
+++ FlickrViewer/ImageCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ImageCollectionViewCell.swift
+//  FlickrViewer
+//
+//  Created by Kirill Shteffen on 04/07/2018.
+//  Copyright © 2018 BlackBricks. All rights reserved.
+//
+
+
+import SDWebImage
+
+class ImageCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var resultImageView: UIImageView!
+
+    func setupWithPhoto(flickrPhoto: Photo) {
+        resultImageView.sd_setImage(with: flickrPhoto.photoUrl as URL?)
+    }
+}
